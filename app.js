31ba519489b085fcb3275894fd2e1435653c4ea4
Rokhaya Ba <rokhaya-jennifer.ba@mines-ales.org>
2021-09-28 23:37:44 +0200

    first functional version of feature

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var express = require("express")
 var app = express();
 var axios = require("axios").default;
 
+//special node module installed to get lyrics
+const Genius = require("genius-lyrics");
+const Client = new Genius.Client("u14zRI3-FNvsIqw0QIIMDx9V7R0y8NkQGXha69ZSxs8jzzR3kx7ZRnHcLSUjVIus");
+
 // serve files from the public directory
 app.use(express.static('./'));
 
@@ -17,6 +21,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/info-on-song', (req, res) => {
+  res.sendFile(__dirname + '/views/info_on_song.html');
+});
+
+app.get('/artist', (req, res) => {
+  res.sendFile(__dirname + '/views/artist.html');
+});
+
+// api requests
+/*
+  search request to api to get search result
+*/
 app.get('/search-song/:searchedItem', (req, res) => {
   console.log("ok");
   console.log(req.params.searchedItem);
@@ -34,10 +50,56 @@ app.get('/search-song/:searchedItem', (req, res) => {
   };
 
   axios.request(options).then(function (response) {
-    console.log(response.data);
     res.send(response.data)
     res.sendStatus(201);
   }).catch(function (error) {
     console.error(error);
   });
 });
+
+/*
+  api call that gets all infos on a song by its id
+*/
+app.get('/get-info-on-song/:songID', (req, res) => {
+  var songID = req.params.songID;
+
+  var options = {
+    method: 'GET',
+    url: 'https://genius.p.rapidapi.com/songs/' + songID,
+    headers: {
+      'x-rapidapi-host': 'genius.p.rapidapi.com',
+      'x-rapidapi-key': 'e17c302178mshe693526e422e203p1a1cedjsn49af64fb3e36'
+    }
+  };
+
+  axios.request(options).then(async function (response) {
+    var songID = response.data.response.song.id
+    console.log(songID);
+
+    var lyrics = await getLyrics(songID);
+    console.log(lyrics);
+
+    if(lyrics !== ""){
+      res.send({
+        data: response.data,
+        song_lyrics: lyrics
+      });
+      res.sendStatus(201);
+    }else{
+      res.send({
+        data: response.data,
+        song_lyrics: ""
+      });
+      res.sendStatus(201);
+    }
+  }).catch(function (error) {
+  	console.error(error);
+  });
+});
+
+async function getLyrics(songID){
+  const song = await Client.songs.get(songID);
+  const lyrics = await song.lyrics();
+  console.log(lyrics);
+  return lyrics;
+}
--- client.js
+++ client.js
@@ -1,15 +1,60 @@
 console.log('Client-side code running');
-//
-// const button = document.getElementById('myButton');
-// button.addEventListener('click', function(e) {
-//   console.log('button was clicked');
-//
-//
-// });
-function searchItem(){
-  var searchInput = document.getElementById('searchItem');
 
-  fetch('/search-song/'+ searchInput.value , {method: 'get'})
+// event listeners
+const searchButton = document.getElementById('searchBtn');
+if(searchButton){
+  searchButton.addEventListener('click', function(e) {
+    var searchInput = document.getElementById('searchItem');
+
+    fetch('/search-song/'+ searchInput.value , {method: 'get'})
+      .then(function(response) {
+        if(response.ok) {
+          console.log('ok');
+          return response.json();
+        }
+        throw new Error('Request failed.');
+      })
+      .then(function(data){
+        console.log(data.response.hits);
+        var searchResults = data.response.hits;
+        var searchResultsDiv = document.getElementById('results');
+        searchResultsDiv.innerHTML = "";
+        for(let searchResult in searchResults){
+          if(searchResults[searchResult].type === "song"
+          && searchResults[searchResult].result.lyrics_state === "complete"){
+
+            console.log(searchResults[searchResult]);
+            var songID = searchResults[searchResult].result.id;
+
+            var songTitle = searchResults[searchResult].result.title;
+            var songArtistName = searchResults[searchResult].result.primary_artist.name;
+            var songImg = searchResults[searchResult].result.song_art_image_url;
+
+            var url = "/info-on-song?" + songID;
+
+            searchResultsDiv.innerHTML = searchResultsDiv.innerHTML
+            + "<a href="+ url + "><div class=\"search-result\">"
+            + "<img class=\"resultImg\" src=" + songImg +" alt=\"\">"
+            + "<div class=\"sear-result-description\">"
+            + "<h4>" + songTitle + "</h4>"
+            + "<h6>" + songArtistName + "</h6></div></div></a>";
+          }
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
+
+}
+
+// funcions
+function getSongInfo(){
+  var songID = window.location.search.substr(1);
+
+  console.log(songID);
+
+  fetch('/get-info-on-song/'+ songID, {method: 'get'})
     .then(function(response) {
       if(response.ok) {
         console.log('ok');
@@ -18,11 +63,41 @@ function searchItem(){
       throw new Error('Request failed.');
     })
     .then(function(data){
-      console.log(data)
-      // var timeToDisplay = "It's "+ data.heure + " : " + data.minute
-      //h1.innerHTML = data;
+      console.log(data);
+      var songNameOutput = document.getElementById('songTitle');
+      var artistNameOutput = document.getElementById('songArtist');
+      var songImgOutput = document.getElementById('songImg');
+      var songLyricsOutput = document.getElementById('songLyrics');
+
+      var songName = data.data.response.song.title;
+      var artistName = data.data.response.song.primary_artist.name;
+      var songImg = data.data.response.song.song_art_image_url;
+      var songLyrics = data.song_lyrics;
+
+      var artistID = data.data.response.song.primary_artist.id;
+
+      var url_to_artist_info = "/artist?" + artistID
+      songNameOutput.innerText = songName;
+
+      artistNameOutput.innerHTML = "<a href=" + url_to_artist_info + ">"
+      + artistName + "</a>";
+
+      songImgOutput.src = songImg;
+
+      if(songLyrics !== ""){
+        songLyricsOutput.innerText = songLyrics
+      }else{
+        songLyricsOutput.innerText = "Aucunes paroles trouvées pour cette chanson" ;
+      }
+
     })
     .catch(function(error) {
       console.log(error);
     });
+}
+
+function getArtistInfo(){
+  var artistID = window.location.search.substr(1);
+
+  console.log(artistID);
 }
