6ebf8b15dd1c7a081aa27c6676a1b919facd2e5d
Dautheville Dylan <surdidi@gmail.com>
2021-09-30 18:29:40 +0200

    Advance on Feature 2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('Server-side code running');
 var express = require("express")
 var template = require("./template")
 var app = express();
+    var axios = require("axios").default;
 
 // serve files from the public directory
 app.use(express.static('./'));
@@ -17,6 +18,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// serve th page of songs artist
+app.get('/songsartist', (req, res) => {
+    res.sendFile(__dirname + '/view/songartist.html');
+});
+
 app.post('/clicked', (req, res) => {
   var time = template.createResponse();
 	/*just to see if I send the data that I want
@@ -25,4 +31,24 @@ app.post('/clicked', (req, res) => {
 	console.log(time);*/
 	res.send(time)
 	res.sendStatus(201);
+});
+
+app.post('/songOfArtist',(req,res) => {
+    console.log(req);
+    var options = {
+          method: 'GET',
+          url: 'https://genius.p.rapidapi.com/artists/16775/songs',
+          headers: {
+            'x-rapidapi-host': 'genius.p.rapidapi.com',
+            'x-rapidapi-key': 'e17c302178mshe693526e422e203p1a1cedjsn49af64fb3e36'
+          }
+        };
+
+        axios.request(options).then(function (response) {
+        	console.log(response.data);
+        	res.send(response.data);
+        	res.sendStatus(200);
+        }).catch(function (error) {
+        	console.error(error);
+        });
 });
--- client.js
+++ client.js
@@ -22,3 +22,25 @@ button.addEventListener('click', function(e) {
       console.log(error);
     });
 });
+const btnArtiste = document.getElementById('btnArtiste');
+
+btnArtiste.addEventListener('click',function(e){
+    console.log("test");
+    fetch('/songOfArtist', {method: 'POST'})
+        .then(function(response) {
+                    if(response.ok) {
+                      console.log('Click was recorded');
+
+                      return response.json();
+                    }
+                    throw new Error('Request failed.');
+                  })
+                  .then(function(data){
+                        console.log(data);
+                      })
+                      .catch(function(error) {
+                        console.log(error);
+                      });
+
+
+});
